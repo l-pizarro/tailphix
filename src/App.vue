dc599e4cf7cad0ecb40619279427e47f50fb8009
l-pizarro <leandro.pizarro@usach.cl>
2019-12-18 15:01:04 -0300

    Testing the library publish for v0.0.3

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <Checkbox :showLabel="true" label="Mostrar solo lo indicado" />
+  </div>
+</template>
+
+<script>
+import Checkbox from "./components/Checkbox.vue"
+export default {
+  name: 'app',
+  components: {
+    Checkbox
+  }
+}
+</script>
---
+++ src/components/Checkbox.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="w-full flex items-center">
+        <div class="w-5 h-5">
+            <button class="w-full h-full border-2 rounded focus:outline-none" v-bind:class="{ 'border-gray-300': !check, 'border-indigo-200 bg-indigo-400': check }" @click="tap()"></button>
+        </div>
+        <span class="ml-4 text-lg" v-bind:class="{ 'text-gray-500': !check, 'text-gray-600': check }" v-if="showLabel">{{label}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'checkbox',
+    props: [
+        "showLabel",
+        "label"
+    ],
+    data() {
+        return {
+            check: false
+        }
+    },
+    methods: {
+        tap() {
+            this.check = !this.check;
+            this.$emit('onCheck');
+        }
+    }
+}
+</script>
